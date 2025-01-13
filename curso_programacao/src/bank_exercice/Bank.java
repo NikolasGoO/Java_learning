65e6a78aadd1382747f0470dd0f9bab12c0ab22a
Nikolas Gomes <nikolasgomes925@gmail.com>
2025-01-13 16:46:41 -0300

    Ajusting Methods Functions

--- curso_programacao/src/bank_exercice/Bank.java
+++ curso_programacao/src/bank_exercice/Bank.java
@@ -1,62 +1,54 @@
 package bank_exercice;
 
 public class Bank {
-	private int Id;
-	private String Name;
-	private double Balance;
-	
-	public Bank(int id, String name) {
-		this.Id = id;
-		this.Name = name;
-		this.Balance = 0;
-	}
-		
-	public int getId() {
-		return Id;
-	}
-	public void setId(int id) {
-		this.Id = id;
-	}
-	public String getName() {
-		return Name;
-	}
-	public void setName(String name) {
-		Name = name;
-	}
-	public double getBalance() {
-		return Balance;
-	}
-	public void setBalance(double balance) {
-		Balance = balance;
-	}
-	
-	public double MakeDeposit(double deposit) {
-		if(deposit < 0) {
-			System.out.print("It is not possible to make a deposit with a negative amount");
-		} else {
-			Balance += deposit;
-		}
-		return Balance;
-	}
-	
-	public double MakeWithdraw(double withdraw) {
-		if(withdraw > Balance) {
-			System.out.print("You dont Have a sufficient Money to make this action.\nPlease Make a Deposit.");
-		}
-		else {
-			Balance -= withdraw + 5;
-		}
-		return Balance;
-	}
+    private int Id;
+    private String Name;
+    private double Balance;
 
-	@Override
-	public String toString() {
-		return "\nAccount "
-				+ Id +
-				", Holder: "
-				+ Name +
-				", Balance: "
-				+ Balance;
-	}
-	
+    public Bank(int id, String name) {
+        this.Id = id;
+        this.Name = name;
+        this.Balance = 0;
+    }
+
+    public int getId() {
+        return Id;
+    }
+    public void setId(int id) {
+        this.Id = id;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public double getBalance() {
+        return Balance;
+    }
+    public void setBalance(double balance) {
+        Balance = balance;
+    }
+
+    public boolean MakeDeposit(double deposit) {
+        if (deposit < 0) {
+            return false;
+        }
+        Balance += deposit;
+        return true;
+    }
+
+    public boolean MakeWithdraw(double withdraw) {
+        double totalWithdraw = withdraw + 5;
+        if (totalWithdraw > Balance) {
+            return false;
+        }
+        Balance -= totalWithdraw;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Account %d, Holder: %s, Balance: %.2f", Id, Name, Balance);
+    }
 }
--- curso_programacao/src/bank_exercice/Main.java
+++ curso_programacao/src/bank_exercice/Main.java
@@ -4,43 +4,48 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter account number: ");
-		int id = sc.nextInt();
-		
-		System.out.println("Enter account holder: ");
-		String holderName = sc.next();
-		
-		System.out.println("Is there na initial deposit (y/n)? ");
-		String response = sc.next();
-		
-		Bank bankAccount = new Bank(id, holderName);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		if(response == "y" || response == "Y") {
-			System.out.println("Enter initial deposit value: ");
-			double initialValue = sc.nextDouble();
-			bankAccount.MakeDeposit(initialValue);
-		}
-		
-		System.out.println("\nAccount data: ");
-		System.out.print(bankAccount.toString());
-		
-		System.out.println("\nEnter a deposit value: ");
-		bankAccount.MakeDeposit(sc.nextDouble());
-		
-		System.out.println("Updated account data: ");
-		System.out.print(bankAccount.toString());
-		
-		System.out.println("Enter a withdraw value: ");
-		bankAccount.MakeWithdraw(sc.nextDouble());
-		
-		System.out.println("Updated account data: ");
-		System.out.print(bankAccount.toString());
-		
-		sc.close();
+        System.out.println("Enter account number: ");
+        int id = sc.nextInt();
 
-	}
+        System.out.println("Enter account holder: ");
+        sc.nextLine(); 
+        String holderName = sc.nextLine();
 
+        System.out.println("Is there an initial deposit (y/n)? ");
+        String response = sc.next();
+
+        Bank bankAccount = new Bank(id, holderName);
+
+        if (response.equalsIgnoreCase("y")) {
+            System.out.println("Enter initial deposit value: ");
+            double initialValue = sc.nextDouble();
+            if (!bankAccount.MakeDeposit(initialValue)) {
+                System.out.println("Deposit value must be positive!");
+            }
+        }
+
+        System.out.println("\nAccount data: ");
+        System.out.println(bankAccount);
+
+        System.out.println("Enter a deposit value: ");
+        if (!bankAccount.MakeDeposit(sc.nextDouble())) {
+            System.out.println("Deposit failed!");
+        }
+
+        System.out.println("Updated account data: ");
+        System.out.println(bankAccount);
+
+        System.out.println("Enter a withdraw value: ");
+        if (!bankAccount.MakeWithdraw(sc.nextDouble())) {
+            System.out.println("Withdraw failed!");
+        }
+
+        System.out.println("Updated account data: ");
+        System.out.println(bankAccount);
+
+        sc.close();
+    }
 }
